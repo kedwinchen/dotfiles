514b7f35484e00f2178e965ced6249253aa787f5
Kedwin Chen <kedwinchen.noreply+git@gmail.com>
2020-11-14 20:08:13 -0800

    20201114 :: polybar :: add modules to bottom bar, fix i3 labels
    - add the modules created earlier to bottom bar
    - fix the i3 labels (displays the index, instead of "%label%")

--- files/.config/polybar/launch.sh
+++ files/.config/polybar/launch.sh
@@ -4,5 +4,10 @@ polybar-msg cmd quit
 killall polybar
 pkill polybar
 
-polybar -r top -c ~/.config/polybar/bars/top.ini &
-polybar -r bottom -c ~/.config/polybar/bars/bottom.ini &
+polybar -r top -c ~/.config/polybar/config.ini &
+polybar -r bottom -c ~/.config/polybar/config.ini &
+
+if [[ $(polybar -m | wc -l) -gt 1 ]] ; then
+    polybar -r ext-top -c ~/.config/polybar/config.ini &
+    polybar -r ext-bottom -c ~/.config/polybar/config.ini &
+fi
